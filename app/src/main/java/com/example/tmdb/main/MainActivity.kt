3e1542975749ee67fb241b2143441815e14dde16
Qaffas <aelqaffasl6@gmail.com>
2020-08-19 11:11:58 +0200

    connected firestore

--- app/src/main/java/com/example/tmdb/main/MainActivity.kt
+++ app/src/main/java/com/example/tmdb/main/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.tmdb.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.tmdb.*
+import com.example.tmdb.R
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity(){
 
@@ -16,6 +19,23 @@ class MainActivity : AppCompatActivity(){
         setContentView(R.layout.activity_main)
 
         setNavigationComponentAdapter()
+
+        val db = FirebaseFirestore.getInstance()
+        val user: MutableMap<String, Any> = HashMap()
+        user["first"] = "Ada"
+        user["last"] = "Lovelace"
+        user["born"] = 1815
+
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(
+                    "l",
+                    "DocumentSnapshot added with ID: " + documentReference.id
+                )
+            }
+            .addOnFailureListener { e -> Log.w("j", "Error adding document", e) }
+
     }
 
     private fun setNavigationComponentAdapter() {
