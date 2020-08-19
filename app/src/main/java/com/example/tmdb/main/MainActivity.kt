eab64fa833f4d1feac4b376ab41aaaefc24fef4c
Qaffas <aelqaffasl6@gmail.com>
2020-08-19 13:16:04 +0200

    Added login screen

--- app/src/main/java/com/example/tmdb/main/MainActivity.kt
+++ app/src/main/java/com/example/tmdb/main/MainActivity.kt
@@ -19,23 +19,6 @@ class MainActivity : AppCompatActivity(){
         setContentView(R.layout.activity_main)
 
         setNavigationComponentAdapter()
-
-        val db = FirebaseFirestore.getInstance()
-        val user: MutableMap<String, Any> = HashMap()
-        user["first"] = "Ada"
-        user["last"] = "Lovelace"
-        user["born"] = 1815
-
-        db.collection("users")
-            .add(user)
-            .addOnSuccessListener { documentReference ->
-                Log.d(
-                    "l",
-                    "DocumentSnapshot added with ID: " + documentReference.id
-                )
-            }
-            .addOnFailureListener { e -> Log.w("j", "Error adding document", e) }
-
     }
 
     private fun setNavigationComponentAdapter() {
